780849a53f7aaf15552cc6544f57ec21bbf83cfa
Jorge Teixeira <jorge92@mailbox.org>
2019-09-02 22:15:57 +0100

    adding react routet to the project

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 import App from './App';
 
 const Root = ({ store }) => (
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <Route path="/" component={App} />
+    </BrowserRouter>
   </Provider>
 );
 
